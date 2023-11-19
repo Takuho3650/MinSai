f02d75d264bee472cf13c627f96a4cf0905b152c
SimakunYamane <simakung@gmail.com>
2023-11-19 13:20:08 +0900

    ピンのロード用関数を追加

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var api_key = "AIzaSyAnrCWfPJDCuOOj9X7uy8eHkdrmB5TUnD4";
 const gmarkers = {};
 var markerId = 0;
-var infoWindow = null;
+var currentInfoWindow = null;
 
 var map;
 
@@ -16,7 +16,6 @@ let contentString =
     '</form>' +
     "</div>" +
     "</div>";
-let infowindow;
 
 // 避難所を示すマーカーリスト
 // markers_shelter = [[lat,lng,name], [lat,lng,name]]
@@ -54,7 +53,9 @@ function View(position) {
     alert(geo_text);
     
     // APIから近辺の避難所情報を取得してピンを立てる
-    
+    const latRange = 0.1;
+    const lngRange = 0.1;
+    loadShelters(map, lat - latRange, lng - lngRange, lat + latRange, lng + lngRange);
 }
 
 function initMap() {
@@ -65,7 +66,7 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"), opts);
 
     //避難所ピンの表示テスト
-    let shelterList = [
+    /*let shelterList = [
         {lat: "35.708984", lng: "139.810603", name:"XX小学校"},
         {lat: "35.707984", lng: "139.810503", name:"XX老人ホーム"},
         {lat: "35.706984", lng: "139.810403", name:"XX病院"},
@@ -74,8 +75,8 @@ function initMap() {
     ]
     for(let i = 0; i < shelterList.length; i++) {
         let infoMap = {"収容人数": "50名", "混雑状況": "空"};
-        placeShelterMarker(map, shelterList[i]["name"], shelterList[i]["lat"], shelterList[i]["lng"], infoMap);
-    }
+        //placeShelterMarker(map, shelterList[i]["name"], shelterList[i]["lat"], shelterList[i]["lng"], infoMap);
+    }*/
 
     //コメントピンの表示テスト
     let commentList = [
@@ -83,15 +84,15 @@ function initMap() {
         {time:"2023/11/18 05:14", comment:"路面の舗装がはがれて通行困難です。", username:"名無しの権兵衛"},
         {time:"2023/11/18 12:28", comment:"ガス管の漏れを発見。"}
     ];
-    placeCommentPin(map, null, 35.707984, 139.800703, commentList);
+    placeCommentMarker(map, null, 35.707984, 139.800703, commentList);
 
     let commentList2 = [
         {time:"2023/11/18 13:05", comment:"急流下りできそう。"},
         {time:"2023/11/18 14:14", comment:"川が増水しています。", username:"名無しの権兵衛"}
     ];
-    placeCommentPin(map, null, 35.707984, 139.797703, commentList2);
+    placeCommentMarker(map, null, 35.707984, 139.797703, commentList2);
 
-    //loadShelters();
+    loadCommentMarkers(map);
 
     marker_prelocate = new google.maps.Marker();
     marker_new = new google.maps.Marker();
@@ -106,81 +107,84 @@ function initMap() {
     ////https://developers.google.com/maps/documentation/javascript/examples/control-positioning
     let markers = [];
     searchBox.addListener("places_changed", () => {
-    ////"place_chaged"イベントはAutoCompleteクラスのイベント.
-    ////https://developers.google.com/maps/documentation/javascript/reference/places-widget#Autocomplete.place_changed
+        ////"place_chaged"イベントはAutoCompleteクラスのイベント.
+        ////https://developers.google.com/maps/documentation/javascript/reference/places-widget#Autocomplete.place_changed
 
-    const places = searchBox.getPlaces();
-    ////"getPlaces"メソッドはクエリ(検索キーワード)を配列(PlaceResult)で返す。
-    ////https://developers.google.com/maps/documentation/javascript/reference/places-widget#Autocomplete.place_changed
+        const places = searchBox.getPlaces();
+        ////"getPlaces"メソッドはクエリ(検索キーワード)を配列(PlaceResult)で返す。
+        ////https://developers.google.com/maps/documentation/javascript/reference/places-widget#Autocomplete.place_changed
 
-    if (places.length == 0) {
-      return;
-    }
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      //"forEach"メソッドは引数にある関数へ、Mapオブジェクトのキー/値を順に代入･関数の実行をする。
-        //Mapオブジェクト:
-          //https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Map
-      marker.setMap(null);
-      ////setMapメソッドはMarker(Polyline,Circleなど)クラスのメソッド。Markerを指定した位置に配置する。引数nullにすると地図から取り除く。
-    });
-    markers = [];
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
-    ////"LatLngBounds"クラスは境界を作るインスンタンスを作成。引数は左下、右上の座標。
-    ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/LatLngBounds/#:~:text=LatLngBounds%E3%82%AF%E3%83%A9%E3%82%B9%E3%81%AF%E5%A2%83%E7%95%8C(Bounding,%E4%BD%9C%E3%82%8B%E3%81%93%E3%81%A8%E3%82%82%E3%81%A7%E3%81%8D%E3%81%BE%E3%81%99%E3%80%82
-    places.forEach((place) => {
-        if (!place.geometry) {
-            ////"geometry"はplaceライブラリのメソッド。
-
-            console.log("Returned place contains no geometry");
+        if (places.length == 0) {
             return;
         }
-        const icon = {
-            url: place.icon,
-            ////"icon"はアイコンを表すオブジェクト。マーカーをオリジナル画像にしたいときなど。
-            ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/Icon/
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            ////"Point"クラスはマーカーのラベルなどの位置を決めるインスタンスメソッド。
-            ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/Point/
+        // Clear out the old markers.
+        markers.forEach((marker) => {
+            //"forEach"メソッドは引数にある関数へ、Mapオブジェクトのキー/値を順に代入･関数の実行をする。
+            //Mapオブジェクト:
+            //https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Map
+            marker.setMap(null);
+        ////setMapメソッドはMarker(Polyline,Circleなど)クラスのメソッド。Markerを指定した位置に配置する。引数nullにすると地図から取り除く。
+        });
+        markers = [];
 
-            scaledSize: new google.maps.Size(25, 25),
-        };
-        // Create a marker for each place.
-        markers.push(
-            new google.maps.Marker({
-                map,
-                icon,
-                title: place.name,
-                position: place.geometry.location,
-            })
-        );
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+        ////"LatLngBounds"クラスは境界を作るインスンタンスを作成。引数は左下、右上の座標。
+        ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/LatLngBounds/#:~:text=LatLngBounds%E3%82%AF%E3%83%A9%E3%82%B9%E3%81%AF%E5%A2%83%E7%95%8C(Bounding,%E4%BD%9C%E3%82%8B%E3%81%93%E3%81%A8%E3%82%82%E3%81%A7%E3%81%8D%E3%81%BE%E3%81%99%E3%80%82
+        places.forEach((place) => {
+            if (!place.geometry) {
+                ////"geometry"はplaceライブラリのメソッド。
 
-        if (place.geometry.viewport) {
-            ////viewport"メソッド
-            // Only geocodes have viewport.
-            bounds.union(place.geometry.viewport);
-            ////"union"メソッドはLatLngBoundsクラスのメソッド。自身の境界に指定した境界を取り込んで合成する。
-            ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/LatLngBounds/union/
-        } else {
-            bounds.extend(place.geometry.location);
-            ////"extend"メソッドはLatLngBoundsクラスのメソッド。自身の境界に新しく位置座標を追加する。
-            ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/LatLngBounds/extend/
-        }
-    });
-    map.fitBounds(bounds);
-    ////"fitBounds"メソッドはmapクラスのメソッド。指定した境界を見えやすい位置にビューポートを変更する。
-    ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/Map/fitBounds/#:~:text=Map.fitBounds()%E3%81%AFMap,%E5%A4%89%E6%9B%B4%E3%81%97%E3%81%A6%E3%81%8F%E3%82%8C%E3%81%BE%E3%81%99%E3%80%82
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            const icon = {
+                url: place.icon,
+                ////"icon"はアイコンを表すオブジェクト。マーカーをオリジナル画像にしたいときなど。
+                ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/Icon/
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                ////"Point"クラスはマーカーのラベルなどの位置を決めるインスタンスメソッド。
+                ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/Point/
+
+                scaledSize: new google.maps.Size(25, 25),
+            };
+            // Create a marker for each place.
+            markers.push(
+                new google.maps.Marker({
+                    map,
+                    icon,
+                    title: place.name,
+                    position: place.geometry.location,
+                })
+            );
+
+            if (place.geometry.viewport) {
+                ////viewport"メソッド
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+                ////"union"メソッドはLatLngBoundsクラスのメソッド。自身の境界に指定した境界を取り込んで合成する。
+                ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/LatLngBounds/union/
+            } else {
+                bounds.extend(place.geometry.location);
+                ////"extend"メソッドはLatLngBoundsクラスのメソッド。自身の境界に新しく位置座標を追加する。
+                ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/LatLngBounds/extend/
+            }
+        });
+        map.fitBounds(bounds);
+        ////"fitBounds"メソッドはmapクラスのメソッド。指定した境界を見えやすい位置にビューポートを変更する。
+        ////https://lab.syncer.jp/Web/API/Google_Maps/JavaScript/Map/fitBounds/#:~:text=Map.fitBounds()%E3%81%AFMap,%E5%A4%89%E6%9B%B4%E3%81%97%E3%81%A6%E3%81%8F%E3%82%8C%E3%81%BE%E3%81%99%E3%80%82
 
     });
-    infowindow = new google.maps.InfoWindow({
-        content: createMessageForm(),
-        ariaLabel: "Comment",
-    });
+    
     marker_new.addListener("click", () => {
-        infowindow.open({
+        const infoWindow = new google.maps.InfoWindow({
+            content: createMessageForm(),
+            ariaLabel: "Comment",
+        });
+
+        infoWindow.open({
             anchor: marker_new,
             map,
         });
@@ -206,8 +210,6 @@ function clickListener(event, map) {
         anchor: marker_new,
         map,
     });
-
-    
 }
 
 //避難場所ピンを設置する。
@@ -215,6 +217,8 @@ function clickListener(event, map) {
 //info: 備考欄に表示する情報リスト なしでも可　{"[項目1]": "[値1]", "[項目2]": "[値2]", ...}
 function placeShelterMarker(map, name, latitude, longitude, info) {
     const tagName = '指定避難場所';
+
+    console.log(longitude);
     
     const iconImgUri = "./shelter.png";
     const markerLatLng = new google.maps.LatLng(latitude, longitude);
@@ -248,13 +252,13 @@ function placeShelterMarker(map, name, latitude, longitude, info) {
         }
         descriptionHTML += '</div>';
         
-        if(infoWindow != null) infoWindow.close();
-        infoWindow = new google.maps.InfoWindow({
+        if(currentInfoWindow != null) currentInfoWindow.close();
+        currentInfoWindow = new google.maps.InfoWindow({
             content: descriptionHTML,
             ariaLabel: name
         });
 
-        infoWindow.open({
+        currentInfoWindow.open({
             anchor: marker,
             map
         });
@@ -286,7 +290,7 @@ function createCommentArea(metadata) {
 //メッセージフォームを作成する
 function createMessageForm(lat, lng) {
     let formHTML = 
-              '<form class="messageform">'
+              '<form class="messageform" onsubmit="sendCommand(' + lat + ', ' + lng + ')">'
             + '  <div class="block">'
             + '    <label for="name">名前</label>'
             + '    <input type="text" id="name" name="name">'
@@ -295,7 +299,7 @@ function createMessageForm(lat, lng) {
             + '    <label for="content">メッセージ</label>'
             + '    <textarea id="message" name="content"></textarea>'
             + '  </div>'
-            + '  <button class="formbutton" onclick="send()">送信</button>'
+            + '  <button class="formbutton" type="submit">送信</button>'
             + '</form>';
 
     return formHTML;
@@ -305,7 +309,7 @@ function createMessageForm(lat, lng) {
 //コメントのピンを設置する
 //name: 地点名 ないときはnull, latitude: 緯度, longitude: 経度
 //comments: 表示するコメントのリスト　[{"time":"[時間]", "comment": "[ユーザーのコメント]", "username": "[ユーザー名(このキーはなくてもよい)]"}, ...]
-function placeCommentPin(map, name, latitude, longitude, commentList) {
+function placeCommentMarker(map, name, latitude, longitude, commentList) {
     const markerLatLng = new google.maps.LatLng(latitude, longitude);
     const marker = new google.maps.Marker({
         position: markerLatLng,
@@ -329,13 +333,13 @@ function placeCommentPin(map, name, latitude, longitude, commentList) {
             + '<button class="formbutton" onclick="onAddComment(' + marker.metadata.markerId + ')">投稿を追加</button>';
             + '</div>';
 
-        if(infoWindow != null) infoWindow.close();
-        infoWindow = new google.maps.InfoWindow({
+        if(currentInfoWindow != null) currentInfoWindow.close();
+        currentInfoWindow = new google.maps.InfoWindow({
             content: commentHTML,
             ariaLabel: marker.metadata.name,
         });
 
-        infoWindow.open({
+        currentInfoWindow.open({
             anchor: marker,
             map,
         });
@@ -352,12 +356,12 @@ function onAddComment(markerId) {
             + createMessageForm(marker.metadata.lat, marker.metadata.lng)
             + '</div>';
 
-    if(infoWindow != null) infoWindow.close();
-    infoWindow = new google.maps.InfoWindow({
+    if(currentInfoWindow != null) currentInfoWindow.close();
+    currentInfoWindow = new google.maps.InfoWindow({
         content: commentHTML,
         ariaLabel: '新規投稿',
     });
-    infoWindow.open({
+    currentInfoWindow.open({
         anchor: gmarkers[markerId],
         map,
     });
@@ -375,18 +379,43 @@ function removePins() {
 
 //避難場所の一覧を取得します。
 function loadShelters(map, latMin, lngMin, latMax, lngMax) {
-    const url = "https://kiwasalog.com/caprice/xhr-cors.html"; //送信先
+    const url = 'http://localhost:8000/shelters/' + latMin + '/' + lngMin + '/' + latMax + '/' + lngMax + '}'; //送信先
 
     let request = new XMLHttpRequest();
     request.onreadystatechange = () => {
         if(request.readyState == 4 && request.status == 200) {
             const response = JSON.parse(request.responseText);
-
+    
             //JSONを適宜加工する
-
-            let shelterList = []; //あとで
+            let shelterList = response.shelters; //あとで
             for(let i = 0; i < shelterList.length; i++) {
-                placeShelterMarker(map, shelterList[i]["name"], shelterList[i]["lat"], shelterList[i]["lng"]);
+                placeShelterMarker(map, shelterList[i][2], shelterList[i][0], shelterList[i][1]);
+            }
+        }
+    }
+
+    request.open("GET", url);
+    request.send();
+}
+
+//現在登録されているピンの一覧を取得します。
+function loadCommentMarkers(map) {
+    const url = 'http://localhost:8000/flags'; //送信先
+
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        console.log("readystate: " + request.readyState);
+        if(request.readyState == 4 && request.status == 200) {
+            const response = JSON.parse(request.responseText);
+            console.log(response);
+    
+            //JSONを適宜加工する
+            let commentMarkerList = response.flags; //あとで
+            console.log(commentMarkerList);
+            for(let i = 0; i < commentMarkerList.length; i++) {
+                console.log(commentMarkerList[i].lat + 30);
+                const comments = [{time: commentMarkerList[i].time, comment: commentMarkerList[i].comments}];
+                placeCommentMarker(map, null, commentMarkerList[i].lat, commentMarkerList[i].lng, comments);
             }
         }
     }
@@ -396,7 +425,8 @@ function loadShelters(map, latMin, lngMin, latMax, lngMax) {
 }
 
 
-function sendComment(lat, lng, username, message) {
+function sendComment(lat, lng) {
+    let message = document.querySelector('#message').value;
     console.log("lat: " + lat + ", lng: " + lng + ", username: " + username + ", message: " + message);
     //送信処理
 }
